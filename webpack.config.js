644892c88c04509a650236f789014e1cfc638167
Christophe THEVENET <christophethevenet2.0@gmail.com>
2023-04-19 10:50:18 +0200

    ci: config webpack encore

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore.setOutputPath('public/build/')
+    .setPublicPath('/build')
+
+    .addEntry('app', './assets/js/index.js')
+    .addStyleEntry('styles', './assets/scss/styles.scss')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = '3.23';
+    })
+
+    .enableSassLoader()
+    .enableReactPreset()
+
+    .enableIntegrityHashes(Encore.isProduction());
+
+module.exports = Encore.getWebpackConfig();
