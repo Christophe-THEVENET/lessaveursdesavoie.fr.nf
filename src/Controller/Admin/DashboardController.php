a5e2242e54980f49156266365d7bc3704a2243f6
Christophe THEVENET <christophethevenet2.0@gmail.com>
2023-04-26 11:31:14 +0200

    fix: security admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -16,10 +16,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
+    #[IsGranted('ROLE_ADMIN', message: 'Impossible d\'accéder aux utilisateurs.')]
     public function index(): Response
     {
         // return parent::index();
