08d563a86ce0d8d4b5b7cbed097831b566e3c95d
Christophe THEVENET <christophethevenet2.0@gmail.com>
2023-04-28 16:40:15 +0200

    style: api controller

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -14,21 +14,16 @@ class ApiController extends AbstractController
 
 
 
-
+    // ------------------- API GET ALL OPENING HOURS ------------------- //
     #[Route('/api/opening-hours', name: 'api_opening_hours')]
     public function getApiOpeningHours(OpeningHoursRepository $openingHoursRepository, SerializerInterface $serializer): JsonResponse
     {
-
         $openingHoursList = $openingHoursRepository->findAll();
-
 
         if (!$openingHoursList) {
             return new JsonResponse(null, Response::HTTP_NOT_FOUND);
         }
-
         $openingHoursListSerialized = $serializer->serialize($openingHoursList, 'json', ['groups' => 'openingHours']);
-        /*   dd($openingHoursListSerialized); */
-
 
         return new JsonResponse($openingHoursListSerialized, Response::HTTP_OK, [], true);
     }
