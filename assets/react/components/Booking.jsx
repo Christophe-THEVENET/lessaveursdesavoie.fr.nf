ee01e415fa8f340f0d429e84f3745c8660a691a6
Christophe THEVENET <christophethevenet2.0@gmail.com>
2023-09-19 16:10:39 +0200

    fix: url prod query user

--- assets/react/components/Booking.jsx
+++ assets/react/components/Booking.jsx
@@ -99,7 +99,7 @@ export default function Booking() {
     const [errorUser, setErrorUser] = useState(false);
     const getUser = async () => {
         try {
-            const response = await axios.get(`https://127.0.0.1:8000/api/user`);
+            const response = await axios.get(`https://lessaveursdesavoie.fr.nf/api/user`);
             setUser(response.data);
         } catch (error) {
             setErrorUser(
